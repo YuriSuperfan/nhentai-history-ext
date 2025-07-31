07c8d556ba8ec3f6bb3056c3a274baf664d9cd2f
YuriSuperfan <yuri.superfan@gmail.com>
2025-07-31 21:54:44 +0200

    fix: single db load for stats

--- pages/stats.js
+++ pages/stats.js
@@ -41,253 +41,258 @@ function makeResultCard(title, nbReads, children, href) {
     return result;
 }
 
-let current = "gallery";
-const pageSize = 10;
+async function setupStats() {
+    let current = "gallery";
+    const pageSize = 10;
+    const historyData = await db.history.toArray();
 
-function setupGalleryStats() {
-    let currentPage = 0;
-    let isLoading = false;
-    let reachedEnd = false;
-    let orderedData = undefined;
+    function setupGalleryStats() {
+        let currentPage = 0;
+        let isLoading = false;
+        let reachedEnd = false;
+        let orderedData = undefined;
 
-    async function loadNextGalleries() {
-        if (isLoading || reachedEnd) {
-            return;
-        }
-        isLoading = true;
+        async function loadNextGalleries() {
+            if (isLoading || reachedEnd) {
+                return;
+            }
+            isLoading = true;
 
-        if (orderedData === undefined) {
-            const data = await db.history.toArray();
-            data.sort((a, b) => {
-                const diff = b.readTimestamps.length - a.readTimestamps.length;
-                if (diff !== 0) return diff;
-                return b.lastRead - a.lastRead;
-            });
+            if (orderedData === undefined) {
+                const data = [...historyData];
+                data.sort((a, b) => {
+                    const diff = b.readTimestamps.length - a.readTimestamps.length;
+                    if (diff !== 0) return diff;
+                    return b.lastRead - a.lastRead;
+                });
 
-            orderedData = data;
-        }
+                orderedData = data;
+            }
 
-        const offset = currentPage * pageSize;
+            const offset = currentPage * pageSize;
 
-        if (offset > orderedData.length) {
-            reachedEnd = true;
+            if (offset > orderedData.length) {
+                reachedEnd = true;
+                isLoading = false;
+                return;
+            }
+
+            orderedData
+                .slice(offset, offset + pageSize)
+                .forEach((gallery) => {
+                    galleryResults.appendChild(makeResultCard(gallery.title, gallery.readTimestamps.length, makeCover(gallery, {lastRead: true})));
+                });
+
+            currentPage++;
             isLoading = false;
-            return;
         }
 
-        orderedData
-            .slice(offset, offset + pageSize)
-            .forEach((gallery) => {
-                galleryResults.appendChild(makeResultCard(gallery.title, gallery.readTimestamps.length, makeCover(gallery, {lastRead: true})));
-            });
+        window.addEventListener('scroll', () => {
+            const scrollTop = window.scrollY;
+            const windowHeight = window.innerHeight;
+            const fullHeight = document.documentElement.scrollHeight;
 
-        currentPage++;
-        isLoading = false;
+            if (scrollTop + windowHeight >= fullHeight - 300) {
+                if (current === "gallery") {
+                    loadNextGalleries();
+                }
+            }
+        });
+
+        galleryButton.addEventListener("click", () => {
+            galleryResults.classList.add("current-results");
+            artistResults.classList.remove("current-results");
+            tagResults.classList.remove("current-results");
+            galleryButton.classList.add("selected");
+            artistButton.classList.remove("selected");
+            tagButton.classList.remove("selected");
+            current = "gallery";
+        });
+
+        loadNextGalleries();
     }
 
-    window.addEventListener('scroll', () => {
-        const scrollTop = window.scrollY;
-        const windowHeight = window.innerHeight;
-        const fullHeight = document.documentElement.scrollHeight;
+    function setupArtistStats() {
+        let currentPage = 0;
+        let isLoading = false;
+        let reachedEnd = false;
+        let orderedData = undefined;
 
-        if (scrollTop + windowHeight >= fullHeight - 300) {
-            if (current === "gallery") {
-                loadNextGalleries();
+        async function loadNextArtists() {
+            if (isLoading || reachedEnd) {
+                return;
             }
-        }
-    });
+            isLoading = true;
 
-    galleryButton.addEventListener("click", () => {
-        galleryResults.classList.add("current-results");
-        artistResults.classList.remove("current-results");
-        tagResults.classList.remove("current-results");
-        galleryButton.classList.add("selected");
-        artistButton.classList.remove("selected");
-        tagButton.classList.remove("selected");
-        current = "gallery";
-    });
+            if (orderedData === undefined) {
+                const data = [...historyData];
 
-    loadNextGalleries();
-}
+                const artistMap = {};
 
-function setupArtistStats() {
-    let currentPage = 0;
-    let isLoading = false;
-    let reachedEnd = false;
-    let orderedData = undefined;
-
-    async function loadNextArtists() {
-        if (isLoading || reachedEnd) {
-            return;
-        }
-        isLoading = true;
-
-        if (orderedData === undefined) {
-            const data = await db.history.toArray();
-
-            const artistMap = {};
-
-            for (const entry of data) {
-                if (!entry.artist ) {
-                    continue;
-                }
-                const artist = entry.artist;
-                const readCount = entry.readTimestamps.length;
-
-                if (!artistMap[artist]) {
-                    artistMap[artist] = {
-                        name: artist,
-                        readNb: 0,
-                        galleries: []
-                    };
-                }
-
-                artistMap[artist].readNb += readCount;
-                artistMap[artist].galleries.push(entry);
-            }
-
-            orderedData = Object.values(artistMap).sort(
-                (a, b) => b.readNb - a.readNb
-            );
-
-            orderedData.forEach((artist) => {
-                artist.galleries.sort((a, b) => b.readTimestamps.length - a.readTimestamps.length)
-            });
-        }
-
-        const offset = currentPage * pageSize;
-
-        if (offset > orderedData.length) {
-            reachedEnd = true;
-            isLoading = false;
-            return;
-        }
-
-        orderedData
-            .slice(offset, offset + pageSize)
-            .forEach((artist) => {
-                artistResults.appendChild(makeResultCard(artist.name, artist.readNb, artist.galleries.map((gallery) => {
-                    return makeCover(gallery, {noDate: true, noOverflow: true, detailReads: true});
-                }), `https://nhentai.net/artist/${artist.name}/`));
-            });
-
-        currentPage++;
-        isLoading = false;
-    }
-
-    window.addEventListener('scroll', () => {
-        const scrollTop = window.scrollY;
-        const windowHeight = window.innerHeight;
-        const fullHeight = document.documentElement.scrollHeight;
-
-        if (scrollTop + windowHeight >= fullHeight - 300) {
-            if (current === "artist") {
-                loadNextArtists();
-            }
-        }
-    });
-
-    artistButton.addEventListener("click", () => {
-        galleryResults.classList.remove("current-results");
-        artistResults.classList.add("current-results");
-        tagResults.classList.remove("current-results");
-        galleryButton.classList.remove("selected");
-        artistButton.classList.add("selected");
-        tagButton.classList.remove("selected");
-        current = "artist";
-    });
-
-    loadNextArtists();
-}
-
-function setupTagStats() {
-    let currentPage = 0;
-    let isLoading = false;
-    let reachedEnd = false;
-    let orderedData = undefined;
-
-    async function loadNextTags() {
-        if (isLoading || reachedEnd) {
-            return;
-        }
-        isLoading = true;
-
-        if (orderedData === undefined) {
-            const data = await db.history.toArray();
-
-            const tagMap = {};
-
-            for (const entry of data) {
-                for (const tag of entry.tags) {
+                for (const entry of data) {
+                    if (!entry.artist) {
+                        continue;
+                    }
+                    const artist = entry.artist;
                     const readCount = entry.readTimestamps.length;
 
-                    if (!tagMap[tag]) {
-                        tagMap[tag] = {
-                            tag: tag,
+                    if (!artistMap[artist]) {
+                        artistMap[artist] = {
+                            name: artist,
                             readNb: 0,
                             galleries: []
                         };
                     }
 
-                    tagMap[tag].readNb += readCount;
-                    tagMap[tag].galleries.push(entry);
+                    artistMap[artist].readNb += readCount;
+                    artistMap[artist].galleries.push(entry);
+                }
+
+                orderedData = Object.values(artistMap).sort(
+                    (a, b) => b.readNb - a.readNb
+                );
+
+                orderedData.forEach((artist) => {
+                    artist.galleries.sort((a, b) => b.readTimestamps.length - a.readTimestamps.length)
+                });
+            }
+
+            const offset = currentPage * pageSize;
+
+            if (offset > orderedData.length) {
+                reachedEnd = true;
+                isLoading = false;
+                return;
+            }
+
+            orderedData
+                .slice(offset, offset + pageSize)
+                .forEach((artist) => {
+                    artistResults.appendChild(makeResultCard(artist.name, artist.readNb, artist.galleries.map((gallery) => {
+                        return makeCover(gallery, {noDate: true, noOverflow: true, detailReads: true});
+                    }), `https://nhentai.net/artist/${artist.name}/`));
+                });
+
+            currentPage++;
+            isLoading = false;
+        }
+
+        window.addEventListener('scroll', () => {
+            const scrollTop = window.scrollY;
+            const windowHeight = window.innerHeight;
+            const fullHeight = document.documentElement.scrollHeight;
+
+            if (scrollTop + windowHeight >= fullHeight - 300) {
+                if (current === "artist") {
+                    loadNextArtists();
                 }
             }
+        });
 
-            orderedData = Object.values(tagMap).sort(
-                (a, b) => b.readNb - a.readNb
-            );
+        artistButton.addEventListener("click", () => {
+            galleryResults.classList.remove("current-results");
+            artistResults.classList.add("current-results");
+            tagResults.classList.remove("current-results");
+            galleryButton.classList.remove("selected");
+            artistButton.classList.add("selected");
+            tagButton.classList.remove("selected");
+            current = "artist";
+        });
 
-            orderedData.forEach((tag) => {
-                tag.galleries.sort((a, b) => b.readTimestamps.length - a.readTimestamps.length)
-            });
-        }
-
-        const offset = currentPage * pageSize;
-
-        if (offset > orderedData.length) {
-            reachedEnd = true;
-            isLoading = false;
-            return;
-        }
-
-        orderedData
-            .slice(offset, offset + pageSize)
-            .forEach((tag) => {
-                tagResults.appendChild(makeResultCard(tag.tag, tag.readNb, tag.galleries.map((gallery) => {
-                    return makeCover(gallery, {noDate: true, noOverflow: true, detailReads: true});
-                }), `https://nhentai.net/tag/${tag.tag.replaceAll(" ", "-")}/`));
-            });
-
-        currentPage++;
-        isLoading = false;
+        loadNextArtists();
     }
 
-    window.addEventListener('scroll', () => {
-        const scrollTop = window.scrollY;
-        const windowHeight = window.innerHeight;
-        const fullHeight = document.documentElement.scrollHeight;
+    function setupTagStats() {
+        let currentPage = 0;
+        let isLoading = false;
+        let reachedEnd = false;
+        let orderedData = undefined;
 
-        if (scrollTop + windowHeight >= fullHeight - 300) {
-            if (current === "tag") {
-                loadNextTags();
+        async function loadNextTags() {
+            if (isLoading || reachedEnd) {
+                return;
             }
+            isLoading = true;
+
+            if (orderedData === undefined) {
+                const data = [...historyData];
+
+                const tagMap = {};
+
+                for (const entry of data) {
+                    for (const tag of entry.tags) {
+                        const readCount = entry.readTimestamps.length;
+
+                        if (!tagMap[tag]) {
+                            tagMap[tag] = {
+                                tag: tag,
+                                readNb: 0,
+                                galleries: []
+                            };
+                        }
+
+                        tagMap[tag].readNb += readCount;
+                        tagMap[tag].galleries.push(entry);
+                    }
+                }
+
+                orderedData = Object.values(tagMap).sort(
+                    (a, b) => b.readNb - a.readNb
+                );
+
+                orderedData.forEach((tag) => {
+                    tag.galleries.sort((a, b) => b.readTimestamps.length - a.readTimestamps.length)
+                });
+            }
+
+            const offset = currentPage * pageSize;
+
+            if (offset > orderedData.length) {
+                reachedEnd = true;
+                isLoading = false;
+                return;
+            }
+
+            orderedData
+                .slice(offset, offset + pageSize)
+                .forEach((tag) => {
+                    tagResults.appendChild(makeResultCard(tag.tag, tag.readNb, tag.galleries.map((gallery) => {
+                        return makeCover(gallery, {noDate: true, noOverflow: true, detailReads: true});
+                    }), `https://nhentai.net/tag/${tag.tag.replaceAll(" ", "-")}/`));
+                });
+
+            currentPage++;
+            isLoading = false;
         }
-    });
 
-    tagButton.addEventListener("click", () => {
-        galleryResults.classList.remove("current-results");
-        artistResults.classList.remove("current-results");
-        tagResults.classList.add("current-results");
-        galleryButton.classList.remove("selected");
-        artistButton.classList.remove("selected");
-        tagButton.classList.add("selected");
-        current = "tag";
-    });
+        window.addEventListener('scroll', () => {
+            const scrollTop = window.scrollY;
+            const windowHeight = window.innerHeight;
+            const fullHeight = document.documentElement.scrollHeight;
 
-    loadNextTags();
+            if (scrollTop + windowHeight >= fullHeight - 300) {
+                if (current === "tag") {
+                    loadNextTags();
+                }
+            }
+        });
+
+        tagButton.addEventListener("click", () => {
+            galleryResults.classList.remove("current-results");
+            artistResults.classList.remove("current-results");
+            tagResults.classList.add("current-results");
+            galleryButton.classList.remove("selected");
+            artistButton.classList.remove("selected");
+            tagButton.classList.add("selected");
+            current = "tag";
+        });
+
+        loadNextTags();
+    }
+
+    setupGalleryStats();
+    setupArtistStats();
+    setupTagStats();
 }
 
-setupGalleryStats();
-setupArtistStats();
-setupTagStats();
+setupStats();
