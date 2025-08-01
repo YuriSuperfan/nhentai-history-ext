dab6ca78067efa0382dd2ae55ffaa8f1947534e4
YuriSuperfan <yuri.superfan@gmail.com>
2025-08-01 13:23:01 +0200

    fix: hover info for clarity

--- utils.js
+++ utils.js
@@ -15,16 +15,17 @@ export function makeCover(data, settings) {
     cover.href = `https://nhentai.net/g/${data.galleryId}`;
     cover.target = "_blank"
     cover.className = "cover-card";
+    cover.title = `${data.galleryId}`
 
     cover.innerHTML = `
         <div class="top">
         ${settings.deleteId !== undefined ? `
-            <button class="action-btn delete-btn">
+            <button class="action-btn delete-btn" title="Delete entry">
               <svg width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M2 2l12 12M14 2L2 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
               </svg>
             </button>
-            <button class="action-btn restore-btn">
+            <button class="action-btn restore-btn" title="Restore entry">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" 
                      stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M21 12a9 9 0 1 1-3.1-6.5" />
